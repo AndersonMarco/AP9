68550d2358e08f2fb3f037f3cb5c5807190301dd
anderson <anderson.marco@gmail.com>
2017-03-08 22:23:10 -0300

    fixing bugs found when cpu_v was synthesized in a fpga board.

--- Processor_FPGA/Processor_Template_VHDL_DE115/kit.vhd
+++ Processor_FPGA/Processor_Template_VHDL_DE115/kit.vhd
@@ -35,16 +35,16 @@ ARCHITECTURE Behavior OF kit IS
           );
    END COMPONENT;
       COMPONENT cpu_v
-      PORT( clk	                            : IN    STD_LOGIC;
-            reset                           : IN    STD_LOGIC;
-            RAM_DATA_IN						: IN	STD_LOGIC_VECTOR(15 DOWNTO 0);
-            RAM_DATA_OUT					: OUT   STD_LOGIC_VECTOR(15 DOWNTO 0);
-            RAM_ADDRESS                     : OUT   STD_LOGIC_VECTOR(15 DOWNTO 0);
-            RW                              : OUT   STD_LOGIC;
-            keyboard                        : IN    STD_LOGIC_VECTOR(7 DOWNTO 0);
+      PORT( wire_clock	                            : IN    STD_LOGIC;
+            wire_reset                           : IN    STD_LOGIC;
+            bus_RAM_DATA_IN						: IN	STD_LOGIC_VECTOR(15 DOWNTO 0);
+            bus_RAM_DATA_OUT					: OUT   STD_LOGIC_VECTOR(15 DOWNTO 0);
+            bus_RAM_ADDRESS                     : OUT   STD_LOGIC_VECTOR(15 DOWNTO 0);
+            wire_RW                              : OUT   STD_LOGIC;
+            bus_keyboard                        : IN    STD_LOGIC_VECTOR(7 DOWNTO 0);
             videoflag                       : OUT   STD_LOGIC;
-            vga_pos                         : OUT   STD_LOGIC_VECTOR(15 DOWNTO 0);
-            vga_char                        : OUT   STD_LOGIC_VECTOR(15 DOWNTO 0);
+            bus_vga_pos                         : OUT   STD_LOGIC_VECTOR(15 DOWNTO 0);
+            bus_vga_char                        : OUT   STD_LOGIC_VECTOR(15 DOWNTO 0);
             data_debug                      : OUT   STD_LOGIC_VECTOR(15 DOWNTO 0)
           );
    END COMPONENT;
@@ -98,6 +98,7 @@ ARCHITECTURE Behavior OF kit IS
 BEGIN
    wire_reset <= NOT(KEY(0));              
     U1: cpu_v PORT MAP (wire_clock_1MHz, wire_reset, bus_RAM_DATA_IN, bus_RAM_DATA_OUT, bus_RAM_ADDRESS, wire_RW, bus_keyboard, wire_videoflag, bus_vga_pos, bus_vga_char,data_debug);    
+	 --U1: cpu PORT MAP (wire_clock_1MHz, wire_reset, bus_RAM_DATA_IN, bus_RAM_DATA_OUT, bus_RAM_ADDRESS, wire_RW, bus_keyboard, wire_videoflag, bus_vga_pos, bus_vga_char);    
     U2: RAM PORT MAP (bus_RAM_ADDRESS, wire_clock_50Mhz, bus_RAM_DATA_OUT, wire_RW, bus_RAM_DATA_IN);
     U3: ps2_keyboard PORT MAP (wire_clock_1KHz, wire_reset, PS2_DAT, PS2_CLK, bus_keyboard);
     U4: vga PORT MAP (wire_clock_50Mhz, wire_clock_25Mhz, wire_reset, wire_videoflag, bus_vga_pos, bus_vga_char, VGA_BLANK_N, VGA_CLK, VGA_VS, VGA_HS, VGA_R, VGA_G, VGA_B);    
